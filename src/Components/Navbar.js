eafb1c0071143387221e8212ba420f911061df6f
Deepak Singh <malwareperseus@gmail.com>
2020-11-15 01:48:41 +0530

    Navbar Skeletal

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,37 @@ import React from 'react';
 const Navbar = () => {
     return(
         <>
-        Navbar
+        <nav className="">
+            <div>
+                <span>D3C4G0N</span>
+            </div>
+            <div>
+                <button>
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+            <div>
+                <div>
+                    <a href='#'>
+                        Home
+                    </a>
+                
+                    <a href='#'>
+                            Home
+                    </a>
+                    <a href='#'>
+                            Home
+                    </a>
+                    <a href='#'>
+                            Home
+                    </a>
+                </div>
+                <div>
+                    <a href="https://discord.gg/fCUmxMxwEg">Join Discord</a>
+                </div>
+
+            </div>
+        </nav>
         </>
     );
 };
