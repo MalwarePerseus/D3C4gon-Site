d39ebc024752b73d2d2d1c0d06ebac690a467bc3
Deepak Singh <malwareperseus@gmail.com>
2020-11-15 02:49:40 +0530

    Navbar Design|Complete

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,33 +3,33 @@ import React from 'react';
 const Navbar = () => {
     return(
         <>
-        <nav className="">
-            <div>
-                <span>D3C4G0N</span>
+        <nav className="flex items-center justify-between flex-wrap p-2">
+            <div className="flex items-center flex-shrink-0 mr-6">
+                <span className="font-semibold text-xl tracking-tight">D3C4G0N</span>
             </div>
-            <div>
+            <div className="block lg:hidden">
                 <button>
-                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
-            <div>
-                <div>
-                    <a href='#'>
+            <div className="w-full flex-grow block lg:flex lg:w-auto lg:items-center">
+                <div className="text-md lg:flex-grow">
+                    <a href='#' className="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
                         Home
                     </a>
                 
-                    <a href='#'>
+                    <a href='#' className="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
                             Home
                     </a>
-                    <a href='#'>
+                    <a href='#' className="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
                             Home
                     </a>
-                    <a href='#'>
+                    <a href='#' className="block mt-4 lg:inline-block lg:mt-0 text-purple-200 hover:text-white mr-4">
                             Home
                     </a>
                 </div>
                 <div>
-                    <a href="https://discord.gg/fCUmxMxwEg">Join Discord</a>
+                    <a className="inline-block text-sm px-4 py-2 leading-none border rounded text-green-200 border-purple-600 hover:border-transparent hover:text-purple-900 hover:bg-white mt-4 lg:mt-0" href="https://discord.gg/fCUmxMxwEg">Join Discord</a>
                 </div>
 
             </div>
