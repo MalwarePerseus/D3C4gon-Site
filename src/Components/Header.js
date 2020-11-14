c07b4d0664c92df5542ddf6e75e08a0111d292ce
Deepak Singh <malwareperseus@gmail.com>
2020-11-15 01:00:58 +0530

    Header | Partial

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Header.css';
+import TyperEff from './TyperEff';
 
 const Header = () => {
     return(
         <>
-            <h1>header</h1>
+            
+            <div className="headermain flex items-center m-12">
+               <div className="w-3/5 headermain"> 
+                <TyperEff />
+                </div>  
+                <div className="w-2/5 ml-12 ">
+                    <img className="decgif" src="./dec.gif" alt=""/>
+                </div>
+            </div>
         </>
     );
 };
